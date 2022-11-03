4c61be2441688b207e0ecb8a606398046146da92
Muhammad Sufyan <sufyanmaqsood92@gmail.com>
2022-11-03 12:09:38 +0500

    promise changed

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 import { start } from 'libp2p-webrtc-star-signalling-server'
 
-const server = await start({
+start({
     port: 13579,
     host: '0.0.0.0',
     metrics: false
-})
-console.log('Signaling server started on: ' + server.info.uri)
-let peers = server.peers()
-console.log('Peers: ' + JSON.stringify(peers))
-setInterval(() => {
-    peers = server.peers()
-    console.log('Peers: ' + JSON.stringify(peers.toString()))
-    try {
-        console.log(Object.keys(peers))
-    } catch (error) {
-        
-    }
+}).then(server => {
+    console.log('Signaling server started on: ' + server.info.uri)
+    let peers = server.peers()
+    console.log('Peers: ' + JSON.stringify(peers))
+    setInterval(() => {
+        peers = server.peers()
+        console.log('Peers: ' + JSON.stringify(peers.toString()))
+        try {
+            console.log(Object.keys(peers))
+        } catch (error) {
+            
+        }
 
-}, 10000)
-// stop when user presses Ctrl+C
-process.on('SIGINT', () => {
-    console.log('Stopping server')
-    server.stop()
-    process.exit()
+    }, 10000)
+    // stop when user presses Ctrl+C
+    process.on('SIGINT', () => {
+        console.log('Stopping server')
+        server.stop()
+        process.exit()
+    })
+}).catch(err => {
+    console.log('Error: ', err)
 })
